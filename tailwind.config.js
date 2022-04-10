5338720a4181eb6872326135ea19cbc1f7055370
Adaji Mukhtar <alfadaji@gmail.com>
2022-04-10 13:28:42 +0100

    created the project and did basit setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        orange: '#FF9A5F',
+        dark: '#1A1A26',
+        greyInput: '#DBBFB3',
+        grey: '#F0F1EE'
+      }
+    },
+  },
+  plugins: [],
+}
